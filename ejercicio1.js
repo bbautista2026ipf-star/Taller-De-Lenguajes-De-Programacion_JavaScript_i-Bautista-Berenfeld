522e9a781cfbf42ae0ac74c850f3fea8d2224dd4
Bautista Berenfeld <bbautista2026ipf@gmail.com>
2026-05-28 09:44:30 -0300

    feat: se realizo el ejercicio numero 1

---
+++ ejercicio1.js
@@ -0,0 +1,16 @@
+let nombre = "Bautista";
+let edad = 18;
+let carrera = "Tecnicatura superior en desarrollo de software multiplataforma";
+let estado_cursado = "cursando primer año";
+
+console.log(
+  "Mi nombre es " +
+    nombre +
+    ", tengo " +
+    edad +
+    " años, estudio " +
+    carrera +
+    " y actualmente estoy " +
+    estado_cursado +
+    ".",
+);
